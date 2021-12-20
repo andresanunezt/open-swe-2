898528f934c389d553ee8b9c06835db05f7588a3
danceoval <daniel.sohval@gmail.com>
2021-12-20 14:38:28 -0500

    adds tdd demo

---
+++ bootcamp/week-1/tdd/demo/index.js
@@ -0,0 +1,9 @@
+class Book {
+	constructor(title, genre){
+		this.title = title
+		this.genre = genre
+	}
+}
+
+//This allows us to access Book in index.test.js
+module.exports = Book
--- bootcamp/week-1/tdd/demo/index.test.js
+++ bootcamp/week-1/tdd/demo/index.test.js
@@ -1,1 +1,25 @@
-console.log("HELLO TESTS!")
+/*
+GETTING STARTED
+npm init
+npm install jest
+find your bliss ;) 
+
+(You can do all this in replit too)
+*/
+
+
+const Book = require('./index') //require whatever we are exporting form index.js
+
+describe('Book class', () => { //Describe: what are we testing?
+
+	const testBook = new Book('Captain Underpants', 'Comedy');	
+
+	test('has as a title', () => { // specific test
+		expect(testBook.title).toBe('Captain Underpants'); //what do we expect this to look like?
+	})
+
+	//how can we test if this book has a genre of 'Comedy'?
+	test('has a genre', () => {
+	  expect(testBook.genre).toBe('Comedy')
+	})
+})
