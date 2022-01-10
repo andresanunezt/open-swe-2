6f26dcb0c6445f595e7584234e20f8749efd65ed
= <90lineng@gmail.com>
2022-01-10 18:08:54 -0500

    sequelize demo added with readme

---
+++ bootcamp/week-3/setup_sequelize_demo/README.md
@@ -0,0 +1,20 @@
+//STEP 1: Create your db and export db, DATATYPES, Model classes
+
+//STEP 2: Create your models and export them {nameOfModel}
+
+//STEP 3: Create some data (.json file), refer to JSON format
+
+//STEP 4: Create your seed file which contains your seed function that will utilize sequelize methods to help us insert our data into as rows in our table
+        //import dependencies that will simplify finding the path to your data
+        //import fs to help us read what file lives in the seedpath
+        //import our database and models
+
+        // create our seed function
+            //clear our db
+            //find our seed paths [x]
+            //read data and parse information in seed files
+
+            // Using the Sequelize method .create, we will insert the rows into our tables
+            // resolve all the promises by using the .all method from the Promise class.
+
+        
--- bootcamp/week-3/setup_sequelize_demo/seed.js
+++ bootcamp/week-3/setup_sequelize_demo/seed.js
@@ -6,6 +6,7 @@ const fs = require('fs').promises //will help me resolve and or reject promises 
 const {db} = require('./db')
 //import our model
 const {Game} = require('./models/Game')
+const {User} = require('./models/User') // [X]
 
 //All sequelize methods return promises -> 
 
@@ -16,27 +17,26 @@ const seed = async () => {
 
     //find our .json file -> parse the data -> JS OBJECT -> Access the array -> create a new row with each element of the array
     const seedPath = path.join(__dirname, 'games.json') 
+    const seedPath2 = path.join(__dirname, 'users.json') // [X]
     
+    //Games
     const buffer = await fs.readFile(seedPath) //reads information that lives in that path
     const {data} = JSON.parse(String(buffer)) // convert the JSON string Object -> JS Object
 
-    //data
-    // [
-    //     {"name": "FF7", "platform": "PS1"},
-    //     {"name": "Spyro", "platform": "PS1"},
-    //     {"name": "Metroid", "platform": "SNES"},
-    //     {"name": "Street Fighter", "platform": "PS1"},
-    //     {"name": "Animal Crossing", "platform": "SWTICH"}
-    // ]
+    //Users
+    const buffer2 = await fs.readFile(seedPath2) //reads information that lives in that path // [X]
+    const {data2} = JSON.parse(String(buffer2)) // convert the JSON string Object -> JS Object // [X]
 
     //1. iterate through our array
     //2. Sequelize method : Model.create(row)
     //3. resolve/reject .all [Promise<pending>,Promise<pending>,Promise<pending>...]
     const gamePromises = data.map(game => Game.create(game))
+    const userPromises = data2.map(user => User.create(user)) // [X]
 
     await Promise.all(gamePromises) //The Promise.all() method takes in an iterable of promises as an input, and returns a single Promise that resolves or rejects
+    await Promise.all(userPromises) // [X]
 
-    console.log(`All of our Games have been successfully populated into our database!`)
+    console.log(`All of our Games and Users have been successfully populated into our database!`)
 }
 
 
